f3123bcf798186c2decf35cf28496a20af94ecee
Rafal Pysz <kuchasz1@o2.pl>
2018-05-01 20:08:05 +0200

    included stylesheets

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Button } from "semantic-ui-react";
-import "semantic-ui-sass/semantic-ui";
 
-ReactDOM.render(<Button>DuDpEczka</Button>, document.querySelector("body"));
+ReactDOM.render(<Button>Click Here!</Button>, document.querySelector("#root"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,74 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve("./src/index.tsx"),
-  output: {
-    filename: "bundle.js"
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader"
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader!sass-loader"
-        })
-      },
-      {
-        test: /\.(png|woff|eot|woff2|eot|ttf|svg)$/,
-        use: "url-loader?limit=100000"
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".scss", ".ts", ".tsx", ".js"]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      alwaysWriteToDisk: true,
-      cache: false
-    }),
-    new ExtractTextPlugin("styles.css")
-  ],
-  devServer: {
-    port: 8080,
-    host: "localhost",
-    historyApiFallback: true,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
+    entry: path.resolve("./src/index.tsx"),
+    output: {
+        filename: "bundle.js"
     },
-    contentBase: path.resolve("./dist"),
-    open: true
-  }
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        "css-loader",
+                        "resolve-url-loader",
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                plugins: [require('autoprefixer')]
+                            }
+                        }, "sass-loader"]
+                })
+            },
+            // this rule handles images
+            // {
+            //     test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+            //     use: 'file-loader?name=[name].[ext]?[hash]'
+            // },
+            //
+            // // the following 3 rules handle font extraction
+            // {
+            //     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            //     loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            // },
+            //
+            // {
+            //     test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            //     loader: 'file-loader'
+            // },
+            // {
+            //     test: /\.otf(\?.*)?$/,
+            //     use: 'file-loader?name=/fonts/[name].  [ext]&mimetype=application/font-otf'
+            // }
+        ]
+    },
+    resolve: {
+        extensions: [".scss", ".ts", ".tsx", ".js"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            alwaysWriteToDisk: true,
+            cache: false
+        }),
+        new ExtractTextPlugin("styles.css")
+    ],
+    devServer: {
+        port: 8080,
+        host: "localhost",
+        historyApiFallback: true,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000
+        },
+        contentBase: path.resolve("./dist"),
+        open: true
+    }
 };
