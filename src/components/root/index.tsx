668a019d8c1c4defb9a984c9c7b37d9fe30f78ac
Rafal Pysz <kuchasz1@o2.pl>
2018-05-02 11:57:36 +0200

    menu rework

--- src/components/root/index.tsx
+++ src/components/root/index.tsx
@@ -1,27 +1,40 @@
 import * as React from "react";
-import {Menu, MenuItemProps} from "semantic-ui-react";
+import {Menu, MenuItemProps, Segment, Sidebar, SemanticCOLORS} from "semantic-ui-react";
 import {Dashboard} from "../dashboard/index";
 
-const menuItems = ['Home', 'Stats', 'Emails', 'Galleries', 'Blogs', 'Comments'];
+const colors: SemanticCOLORS[] = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black'];
+
+const menuItems = [
+    {icon: "home", text: 'Home'},
+    {icon: "signal", text: 'Stats'},
+    {icon: "mail outline", text: 'Emails'},
+    {icon: "leaf", text: 'Galleries'},
+    {icon: "flag", text: 'Blogs'},
+    {icon: "comments", text: 'Comments'},
+    {icon: "log out", text: 'Logout'}
+];
 
 export class Root extends React.Component {
-    state: {activeItem: string} = { activeItem: 'Home' };
+    state: { activeItem: string } = {activeItem: 'Home'};
 
-    handleItemClick = (e: any, { name }: MenuItemProps) => this.setState({ activeItem: name })
+    handleItemClick = (e: any, {name}: MenuItemProps) => this.setState({activeItem: name});
 
     render() {
-        const { activeItem } = this.state;
+        const {activeItem} = this.state;
 
         return (
-            <div>
-                <Menu pointing secondary>
-                    {menuItems.map(mi => <Menu.Item key={mi} name={mi} active={activeItem===mi} onClick={this.handleItemClick} />)}
-                    <Menu.Menu position='right'>
-                        <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
-                    </Menu.Menu>
-                </Menu>
-                    <Dashboard/>
-            </div>
+            <Sidebar.Pushable as={Segment}>
+                <Sidebar as={Menu} animation='uncover' visible={true} icon='labeled' vertical
+                         inverted>
+                    {menuItems.map((mi, id) => <Menu.Item key={mi.text} name={mi.text} icon={mi.icon} color={colors[id]} active={activeItem === mi.text}
+                                                    onClick={this.handleItemClick}></Menu.Item>)}
+                </Sidebar>
+                <Sidebar.Pusher>
+                    <Segment basic>
+                        <Dashboard/>
+                    </Segment>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
         )
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {Root} from "./components/root/index";
+import "./index.scss";
 
 ReactDOM.render(<Root/>, document.querySelector("#root"));
