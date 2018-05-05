18ceb439373991d686589de528d5d422a1db5669
Rafal Pysz <kuchasz1@o2.pl>
2018-05-05 23:05:33 +0200

    changed layout

--- src/components/root/index.tsx
+++ src/components/root/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import {Route} from "react-router";
-import {Segment, Sidebar} from "semantic-ui-react";
 import {Dashboard} from "../dashboard/index";
 import {Menu} from "../menu/index";
 import {Galleries} from "../galleries/index";
@@ -8,16 +7,14 @@ import {Galleries} from "../galleries/index";
 export class Root extends React.Component {
     render() {
         return (
-            <div>
-                <Sidebar visible style={{width: 'auto'}}>
+            <div id="layout">
+                <div>
                     <Menu/>
-                </Sidebar>
-                <Sidebar.Pusher>
-                    <Segment basic>
-                        <Route exact path='/' component={Dashboard}/>
-                        <Route exact path='/galleries' component={Galleries}/>
-                    </Segment>
-                </Sidebar.Pusher>
+                </div>
+                <div>
+                    <Route exact path='/' component={Dashboard}/>
+                    <Route exact path='/galleries' component={Galleries}/>
+                </div>
             </div>
         )
     }
