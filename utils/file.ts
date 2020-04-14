08b9c7c396bd8d2b1ed4a9e8d501e75316bda0ad
root <kuchasz1@o2.pl>
2020-04-15 01:52:25 +0200

    assets selector perf improvement init

--- utils/file.ts
+++ utils/file.ts
@@ -3,14 +3,48 @@ export const read = (file: File): Promise<ReadResult> => {
     return new Promise<ReadResult>((res, rej) => {
         const reader = new FileReader();
 
+        const img = document.createElement("img");
+
         reader.onload = (ev) => {
-            res({ url: reader.result as string, file });
+            img.src = reader.result as string;
+
+            img.onload = () => {
+                const canvas = document.createElement("canvas");
+                const ctx = canvas.getContext("2d");
+                ctx?.drawImage(img, 0, 0);
+
+                const MAX_WIDTH = 100;
+                const MAX_HEIGHT = 100;
+                let width = img.width;
+                let height = img.height;
+
+                if (width > height) {
+                    if (width > MAX_WIDTH) {
+                        height *= MAX_WIDTH / width;
+                        width = MAX_WIDTH;
+                    }
+                } else {
+                    if (height > MAX_HEIGHT) {
+                        width *= MAX_HEIGHT / height;
+                        height = MAX_HEIGHT;
+                    }
+                }
+                canvas.width = width;
+                canvas.height = height;
+                ctx?.drawImage(img, 0, 0, width, height);
+
+                // const dataurl = canvas.toDataURL("image/png");
+
+                //res({ url: dataurl as string, file });
+            };
         };
 
         reader.onerror = (ev) => {
             rej(reader.error);
         };
 
-        reader.readAsDataURL(file);
+        // reader.readAsDataURL(file);
+
+        res({ url: file.name, file });
     });
 };
