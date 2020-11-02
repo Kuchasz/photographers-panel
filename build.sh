eced963a524ee51fc093222b52b3b87deda463aa
Kuchasz <kuchasz1@o2.pl>
2020-11-02 19:37:22 +0100

    build init

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd api
+yarn install
+cd ../gallery
+yarn install
+cd ../gallery-server
+yarn install
+cd ../panel
+yarn install
+cd ../server
+yarn install
+cd ../site
+yarn install
+cd ..
