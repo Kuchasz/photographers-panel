06f96761d5c729813a4daf4e4106684c3de168e7
Kuchasz <kuchasz1@o2.pl>
2020-12-08 23:35:40 +0100

    migrate page visits to site vitits

---
+++ server/src/migrations/12-page-visits-to-site-visits.ts
@@ -0,0 +1,16 @@
+import Knex from "knex";
+import { tableExists, renameTable } from "../core/db";
+
+export const run = async (connection: Knex): Promise<boolean> => {
+    try {
+        if (!(await tableExists("PageVisit", connection))) {
+            return false;
+        }
+
+        await renameTable("PageVisit", "SiteVisit", connection);
+
+        return true;
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
