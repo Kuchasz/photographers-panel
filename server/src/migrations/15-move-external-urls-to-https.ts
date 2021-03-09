11af3990a9bb86dc032b0ddb0cf9dcb3ca4a1c8b
Kuchasz <kuchasz1@o2.pl>
2021-03-09 23:21:23 +0100

    https migration fix

--- server/src/migrations/15-move-external-urls-to-https.ts
+++ server/src/migrations/15-move-external-urls-to-https.ts
@@ -3,6 +3,12 @@ import { runQuery } from "../core/db";
 
 export const run = async (connection: Knex): Promise<boolean> => {
     try {
+        const privateGalleriesWithWrongDirectPath = await connection("PrivateGallery")
+            .where("DirectPath", "like", "http://%")
+            .select<number[]>("PrivateGallery.Id");
+
+        if (privateGalleriesWithWrongDirectPath.length === 0)
+            return false;
 
         await runQuery(`UPDATE "PrivateGallery" SET "DirectPath" = TRIM(REPLACE("DirectPath", 'http://', 'https://'));`, connection);
 
