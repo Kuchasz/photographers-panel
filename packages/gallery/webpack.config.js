260b36c38d32236484a8496a7af9f39e43665859
Rafal Pysz <kuchasz1@o2.pl>
2022-03-04 00:44:32 +0100

    prod gallery fix 4

--- packages/gallery/webpack.config.js
+++ packages/gallery/webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const AngularWebpackPlugin = require('@ngtools/webpack').AngularWebpackPlugin;
+const linkerPlugin = require('@angular/compiler-cli/linker/babel');
 require('dotenv').config({
     path: resolve('../.env'),
 });
