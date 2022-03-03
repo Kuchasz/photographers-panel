e778bb8eb3706fccfbbb81a60bb6d800cd614a01
Rafal Pysz <kuchasz1@o2.pl>
2022-03-03 01:35:21 +0100

    build tweak 76

--- packages/server/src/config/env.ts
+++ packages/server/src/config/env.ts
@@ -1,6 +1,11 @@
 import { config as setConfig } from "dotenv";
 import { resolve } from "path";
 
-setConfig({ path: resolve(__dirname + '../../../../../.env') });
+setConfig({
+    path:
+        process.env === ('production' as any)
+            ? resolve(__dirname + '../../.env')
+            : resolve(__dirname + '../../../../../.env'),
+});
 
 export const getEnv = () => process.env;
