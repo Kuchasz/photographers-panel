14cadd004627359092afd72fbbd0224f987ded80
Rafal Pysz <kuchasz1@o2.pl>
2022-03-22 22:48:39 +0100

    verify middleware added to get events action

--- packages/server/src/areas/routes.ts
+++ packages/server/src/areas/routes.ts
@@ -1,10 +1,11 @@
 import * as event from "@pp/api/dist/event";
 import * as eventController from "./event";
 import { Router as createRouter, Router } from "express";
+import { verify } from "../auth";
 
 const r = createRouter();
 
 r.post(event.reqisterEvent.route, eventController.registerEvent);
-r.get(event.getEventsList.route, eventController.getEventsList);
+r.get(event.getEventsList.route, verify, eventController.getEventsList);
 
 export const router = r as Router;
