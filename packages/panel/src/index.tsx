aa7a8bcfdbaa482e1e42057b7343974404572e8f
Kuchasz <kuchasz1@o2.pl>
2025-02-25 00:43:03 +0100

    wip

--- packages/panel/src/index.tsx
+++ packages/panel/src/index.tsx
@@ -1,10 +1,10 @@
+import 'chartist/dist/scss/chartist.scss';
 import * as React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'rsuite/lib/styles/index.less';
 import { Root } from './components/root/index';
 import './index.less';
-import 'rsuite/lib/styles/index.less';
-import 'chartist/dist/scss/chartist.scss';
-import { BrowserRouter } from 'react-router-dom';
 
 render(
     <BrowserRouter basename="/panel">
