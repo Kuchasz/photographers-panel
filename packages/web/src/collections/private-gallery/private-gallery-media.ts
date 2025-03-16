1a2a4b66992e7e5052896d31b38610c01f48da7e
Kuchasz <kuchasz1@o2.pl>
2025-03-16 16:22:05 +0100

    wip

---
+++ packages/web/src/collections/private-gallery/private-gallery-media.ts
@@ -0,0 +1,39 @@
+import type { CollectionConfig } from 'payload'
+import { PRIVATE_GALLERY_MEDIA_SLUG } from '../collectionSlugs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export const PrivateGalleryMedia: CollectionConfig = {
+  slug: PRIVATE_GALLERY_MEDIA_SLUG,
+  access: {
+    read: () => true,
+  },
+  admin: {
+    useAsTitle: 'filename',
+    group: 'Offers',
+  },
+  upload: {
+    staticDir: path.resolve(dirname, '../../public/private-gallery'),
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 320,
+        position: 'centre',
+        fit: 'contain',
+      },
+      {
+        name: 'big',
+        width: 1920,
+        height: 1920,
+        position: 'centre',
+        fit: 'inside'
+
+      },
+    ],
+    mimeTypes: ['image/png', 'image/jpeg', 'image/jpg', 'image/webp'],
+  },
+  fields: [],
+} 
