dfc1c3f31aefaa456e03089d87ab714282c9d24e
Kuchasz <kuchasz1@o2.pl>
2025-04-18 17:12:24 +0200

    package bump

--- .github/scripts/deploy-test.sh
+++ .github/scripts/deploy-test.sh
@@ -48,7 +48,6 @@ cp ../../env-vars/test.pyszstudio.pl.env .env
 
 echo "🔧 Installing dependencies..."
 pnpm install
-pnpm install sharp@0.34.1
 
 echo "🔄 Restarting web service..."
 devil www restart test.pyszstudio.pl
