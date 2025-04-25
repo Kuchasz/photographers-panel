d42ca6f94cf50c905fd30962add284c7825e9f7c
Kuchasz <kuchasz1@o2.pl>
2025-04-25 16:55:06 +0200

    build fix .2

--- .github/scripts/deploy-test.sh
+++ .github/scripts/deploy-test.sh
@@ -48,7 +48,6 @@ cp ../../env-vars/test.pyszstudio.pl.env .env
 
 echo "🔧 Installing dependencies..."
 pnpm install
-pnpm install sharp@0.32.6 --unsafe-perm
 
 echo "🔄 Restarting web service..."
 devil www restart test.pyszstudio.pl
