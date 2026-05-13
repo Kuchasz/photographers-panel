b8014cc2c96e7c40c1fff5a0cb3e86db63c4b384
Kuchasz <kuchasz1@o2.pl>
2026-05-14 01:51:19 +0200

    packages bump

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -2,21 +2,23 @@
 
 import { revalidateTag } from 'next/cache'
 
+const expireImmediately = { expire: 0 } as const
+
 export const revalidatePhotos = async () => {
-    revalidateTag('photos');
-    revalidateTag('photos-page');
-    revalidateTag('featured-photos');
+    revalidateTag('photos', expireImmediately)
+    revalidateTag('photos-page', expireImmediately)
+    revalidateTag('featured-photos', expireImmediately)
 }
 
 export const revalidateOpinions = async () => {
-    revalidateTag('opinions')
+    revalidateTag('opinions', expireImmediately)
 }
 
 export const revalidateVideos = async () => {
-    revalidateTag('featured-videos')
-    revalidateTag('videos')
+    revalidateTag('featured-videos', expireImmediately)
+    revalidateTag('videos', expireImmediately)
 }
 
 export const revalidateInstagram = async () => {
-    revalidateTag('instagram-posts')
+    revalidateTag('instagram-posts', expireImmediately)
 }
